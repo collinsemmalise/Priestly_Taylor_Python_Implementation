0aedd12c750584e90e505224781ca0fdcc1af619
Emma Collins <32817480+collinsemmalise@users.noreply.github.com>
2018-11-28 11:52:34 -0500

    Updated documentation for Priestley-Taylor module

--- PriestlyTaylorET.py
+++ PriestlyTaylorET.py
@@ -4,9 +4,8 @@
 Created on Fri Jun  1 10:32:31 2018
 
 @author: Emma Collins
-Last Updated: August 16, 2018
-Priestly Taylor Modeling for Evapotranspiration
-Based on the Equations presented by Priestly and Taylor (1972)
+Last Updated: November 5, 2018
+Priestly Taylor Modeling for Potential Evapotranspiration
 """
 
 import numpy as np
@@ -14,74 +13,149 @@ import matplotlib.pyplot as plt
 import matplotlib.dates
 import datetime
 
-#input: albedo constant, solar radiation ((MJ/m**2)/d), cloudiness factor, net
-#       emissivity, Temperature (Kelvin)
-#output: net radiation ((MJ/m**2)/day)
+
 def calculate_net_radiation(albedo, Rs, C, epsilon, T_k):
+    """
+    Calculates Net Solar Radiation in MJ/m^2/day and returns value or list
+    
+    keyword arguments:
+        albedo = albedo of area
+        Rs = solar radiation in MJ/m^2/day
+        C = cloudiness factor
+        epsilon = Net Emissivity
+        T_k = Temperature (Kelvin)
+    """
     o = 4.89*10**-9
     Rn = (1-albedo)*Rs-C*epsilon*o*((T_k)**4)
     return Rn
 
-#input: solar radiation (MJ/m**2/d), extraterrestrial radiation (MJ/m**2/d), ac constant, bc constant
-#output: cloudiness factor
+
 def calculate_cloudiness(Rs, Ra,ac=0.72, bc=0.28):
+    """
+    Calculates Cloudiness Factor and returns value or numpy array
+    
+    keyword arguments:
+        Rs = Solar radiation in MJ/m^2/day
+        Ra = Extraterrestrial Solar Radiation in MJ/m^2/day
+        ac = constant (default is 0.72)
+        bc = constant (default is 0.28)
+    """
     Ra = np.transpose(Ra)[0]
     R = np.divide(Rs, Ra, out=np.zeros_like(Rs), where=Ra!=0)
     return ac*R+bc
 
-#input: Temperature (˚C)
-#output: slope of vaporization curve at temp T (kPa/˚C)
+
 def calculate_Delta(T):
+    """ 
+    Calculates Slope of Vaporization Curve (in kPa/˚C) 
+        at Temperature T (from Tetens, 1930) and returns value or numpy array
+    
+    key arguments:
+        T = Temperature in Celsius 
+    """
     Delta = 4098*(0.6108*np.exp((17.27*T)/(T+237.3)))/(T+237.3)**2
     return Delta
 
-#input: psychrometric constant, temperature of top and bottom, vapor
-#       pressure of top and bottom
-#output: Bowen's ratio (K/kPa) or (˚C/kPa)
+
 def calculate_Bowen(gamma, T2,T1,e2,e1):
+    """
+    Calculates Approximation of Bowen's Ratio (Drexler, 2004) 
+    and returns value or list
+    
+    key arguments:
+        gamma = psychrometric constant
+        T2 = temperature of higher elevation (Celsius or Kelvin)
+        T1 = temperature of lower elevation (Celsius or Kelvin)
+        e2 = vapor pressure of higher elevation (kPa)
+        e1 = vapor pressure of lower elevation (kPa)
+    """
     return gamma*((T2-T1)/(e2-e1))
 
-#input: slope of vaporization curve, psychrometric constant, Bowen's Ratio
-#output: saturation deficit factor
+
 def calculate_alpha(Delta, gamma, beta):
+    """ 
+    Calculates Saturation Deficit Factor and returns value or list
+    
+    key arguments:
+        Delta = Slope of Vaporization Curve
+        gamma = psychrometric constant
+        beta = Bowen's Ratio
+    """
     return (Delta+gamma)/(Delta*(1+beta))
 
-#input: Pressure (kPa), latent heat of vaporization (lambda)
-#output: psychrometric constant (kPa/˚C)
+
 def psychrometric_constant(P, lam):
+    """
+    Calculates Psychrometric Constant and returns value or list (kPa/˚C)
+    
+    key arguments:
+        P = pressure (kPa)
+        lam = latent heat of vaporization
+    """
     return 0.00163*P/lam
 
-#input: temperature (deg Celsius)
-#output: vapor pressure (kPa)
+
 def calculate_vapor_pressure(T):
+    """
+    Calculates Saturation Vapor Pressure (kPa) and returns value or numpy array
+    
+    key arguments:
+        T = Temperature (Celsius) 
+    """
     e = 0.6108*np.exp(17.27*T/(237.3+T))
     return e
 
-#input: air temperature (deg Celsius), relative humidity (%)
-#output: actual vapor pressure (kPa)
+
 def calculate_actual_vapor_pressure(T, RH):
+    """ 
+    Calculates Actual Vapor (kPa) and returns value or numpy array
+    
+    key arguments:
+        T = Tempertaure (Celsius)
+        RH = Relative Humidity  (%)
+    """
     es = calculate_vapor_pressure(T)
     e = RH*es/100
     return e
 
-#input: saturation deficit constant, slope of vaporization curve,
-#       psychrometric constant, net solar radiation
-#output: evapotranspiration (MJ/m**2/day)
+
 def Priestley_Taylor(alpha, Delta, gamma, Rn):
+    """ 
+    Calculates Evapotranspiration (MJ/m^2/day) and returns value or list
+    
+    key arguments:
+        alpha = saturation deficit constant
+        Delta = slope of vaporization curve
+        gamma = psychrometric constant
+        Rn = net solar radiation
+    """
     return alpha*((Delta)/(Delta+gamma))*Rn
 
-# Calculate Net emissivity
-# input: temperature
-# output: net emissivity
 def net_Emissivity(T):
+    """
+    Calculates Net Emissivity and returns value or numpy array
+    
+    key arguments:
+        T = Temperature (Celsius)
+    """
     return 0.261*np.exp(-7.77*10**-4*T**2)-0.02
 
 
-#input: air temp(Celsius), Fuel temp (Celsius), elevation(m), RH(%)
-#       fuel moisture (%), net solar radiation, extraterrestrial solar radiation
-#       albedo constant
-#output: ET
 def PET(air_T, fuel_T, elevation,RH, fuel_moist, Rs, Ra, albedo):
+    """
+    Calculates Potential ET using Priestley-Taylor Equations 
+    (Priestley and Taylor 1972) and returns value or list
+    
+    key arguments:
+        air_T = air temperature (Celsius)
+        fuel_T = fuel temperature or temperature of ground (Celsius)
+        elevation = elevation of ground surface (meters)
+        RH = relative humidity (%)
+        fuel_moist = fuel moisture or moisture of ground (%)
+        Rs = net solar radiation (MJ/m^2/day)
+        Ra = extraterrestrial solar radiation (MJ/m^2/day)
+        albedo = albedo constant
+    """
     P = 101.3 - 0.01055*elevation
     lam = 2.501 - 0.002361*air_T
     Delta = calculate_Delta(air_T)
@@ -97,20 +171,31 @@ def PET(air_T, fuel_T, elevation,RH, fuel_moist, Rs, Ra, albedo):
     ET = Priestley_Taylor(alpha, Delta, gamma, Rn)
     return ET
 
-#Graphs the results of PET estimation
-# input: dates for the x values (as yyyy-mm-dd hh:mm), PET estimates for y values, optional title, y label, and xlabel
-def graph_ET_results(date_time, evapotranspiration, title = "Evapotranspiration", ylabel = 'mm', xlabel = 'Date'):
+def graph_ET_results(date_time, evapotranspiration, title = "Evapotranspiration", ylabel = 'mm', xlabel = 'Date', verbose = True):
+    """
+    Graphs PET results and returns matplotlib dates object from x axis
+    
+    key arguments:
+        date_time = pandas series of Timestamp objects 
+        evapotranspiration = PET estimates corresponding with Timestamps
+        title = title for graph (default is "Evapotranspiration")
+        ylabel = y axis label for graph (default is 'mm')
+        xlabel = x axis label for graph (default is 'Date')
+        verbose(bool) if True, displays graph (default is true)
+    """
+    
     dates = []
     for i in range(len(date_time)):
-        h = datetime.datetime.strptime(date_time[i], "%Y-%m-%d %H:%M")
-        dates.append(datetime.datetime.date(h))
+        dates.append(datetime.datetime.date(date_time.iloc[i]))
     dates = sorted(list(set(dates)))
     dates_plot = matplotlib.dates.date2num(dates)
-    plt.figure(figsize = (15,10))
-    plt.plot_date(dates_plot, np.asarray(evapotranspiration),'-', ydate = False)
-    plt.title(title)
-    plt.xticks(rotation = 'vertical')
-    plt.ylabel(ylabel)
-    plt.xlabel(xlabel)
-    plt.show()
+    if(verbose):
+        plt.figure(figsize = (15,10))
+        plt.plot_date(dates_plot, np.asarray(evapotranspiration),'-', ydate = False)
+        plt.title(title)
+        plt.xticks(rotation = 'vertical')
+        plt.ylabel(ylabel)
+        plt.xlabel(xlabel)
+        plt.show()
     return dates
+    
